a61a5ed29dda133832b8d1ab722215ec85eeab65
Erik Hennerfors <erik.hennerfors@adeprimo.se>
2016-05-10 22:13:54 +0200

    Update, added utility controller for ip fetching

---
+++ Obscured.Holdr.Web/Controllers/UtilityController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Obscured.Holdr.Web.Controllers
+{
+    public class UtilityController : Controller
+    {
+        public ActionResult Ip()
+        {
+            var jsonObj = new
+            {
+                ip = Request.UserHostAddress,
+                hostname = Request.UserHostName,
+                agent = Request.UserAgent,
+                vars = new Dictionary<string, string>()
+            };
+            
+            foreach (var s in Request.ServerVariables.AllKeys)
+            {
+                jsonObj.vars.Add(s, Request.ServerVariables[s]);
+            }
+
+            return Json(jsonObj, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
